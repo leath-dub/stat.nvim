39779149f148374d0d5108f0ced34c48138eafe3
leath-dub <77889270+leath-dub@users.noreply.github.com>
2023-01-04 18:22:44 +0000

    test

--- lua/mod.lua
+++ lua/mod.lua
@@ -73,6 +73,9 @@ function M.git_diff()
   local insertions, deletions = M.git_diff_output[1], M.git_diff_output[2]
   if insertions and not (insertions == "0") then result = result .. "+" .. insertions .. " " end
   if deletions and not (insertions == "0") then result = result .. "-" .. deletions end
+  if not result == "" then
+    return " " .. result .. " "
+  end
   return result
 end
 
